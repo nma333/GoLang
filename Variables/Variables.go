f8ac64b1eea1c9d33da4000383f44f8646a9e17e
Naveen Mahantesh Ankasadaddi <73681675+Naveen-Mahantesh-Ankasadaddi@users.noreply.github.com>
2021-09-26 14:50:01 +0530

    Create Variables.go

---
+++ Variables/Variables.go
@@ -0,0 +1,23 @@
+//Variables in Golang.
+package main
+
+import "fmt"
+
+func main() {
+	//3-Types of Creating a variables.
+	var num int
+	num = 10
+	fmt.Println(num)
+
+	var name string = "Naveen"
+	fmt.Println(name)
+
+	not := true
+	fmt.Println(not)
+
+}
+
+Output:
+      10
+      Naveen
+      true
