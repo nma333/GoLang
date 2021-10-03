bf1260828d80684f8d13e412bacb7479ff834ffa
Naveen Mahantesh Ankasadaddi <73681675+Naveen-Mahantesh-Ankasadaddi@users.noreply.github.com>
2021-10-03 18:28:32 +0530

    Create Constants.go

---
+++ 1.Variables-Constant-Types/Constants.go
@@ -0,0 +1,14 @@
+//Creating the Constant in Golang.
+package main
+
+import "fmt"
+
+//Creating the Constnt.
+const Class string = "BEC"
+
+func main() {
+	fmt.Printf("%v,%T", Class, Class)
+}
+
+Output:
+      BEC,string
