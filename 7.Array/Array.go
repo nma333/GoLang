3ee52d3be48bd58cda43c6d68b6dbc4dd56ad58f
Naveen Mahantesh Ankasadaddi <73681675+Naveen-Mahantesh-Ankasadaddi@users.noreply.github.com>
2021-10-03 19:25:12 +0530

    Update Array.go

--- 7.Array/Array.go
+++ 7.Array/Array.go
@@ -1,1 +1,16 @@
+package main
 
+import "fmt"
+
+func main() {
+	var num [5]float32
+	num[0] = 1.2
+	num[4] = 1.90
+  fmt.Printl("The array elements are:")
+	fmt.Println(num)
+
+}
+
+Output:
+      The array elements are:
+      [1.2 0 0 0 1.9]
